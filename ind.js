2bf47cfc2c3f025f10cfaafc3a1362caaba1b5a7
Shalini <vijipaulshalini@gmail.com>
2026-04-01 21:13:32 +0530

    js

---
+++ ind.js
@@ -0,0 +1,4 @@
+console.log("hello guys");
+window.alert("BTS IS BACK");
+document.getElementById("h1").textContent = "hello army this is vp ";
+document.getElementById("p").textContent = "i am also big fan of BTS THANK GUYS AND LETS SWIM TOGETHER";
