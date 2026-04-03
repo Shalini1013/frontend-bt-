9c755d7e7f0e66f693b82c5b8b9f2ee2e232ccec
Shalini <vijipaulshalini@gmail.com>
2026-04-03 20:25:39 +0530

    d-1

--- ind.js
+++ ind.js
@@ -2,3 +2,13 @@ console.log("hello guys");
 window.alert("BTS IS BACK");
 document.getElementById("h1").textContent = "hello army this is vp ";
 document.getElementById("p").textContent = "i am also big fan of BTS THANK GUYS AND LETS SWIM TOGETHER";
+let age=window.prompt("enter your age:");
+console.log(age);
+let s=10;
+console.log(`the value of s is ${s}`);
+document.getElementById("sub").onclick=function(){
+    let v=document.getElementById("t").value;
+console.log(v)
+document.getElementById("h1").textContent=`hello ${v}`;
+    console.log("button clicked");
+};
